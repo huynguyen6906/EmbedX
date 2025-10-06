009ec88706da554490265d3b1c41197319f46f6b
huynguyen6906 <dinhhuy6906@gmail.com>
2025-10-06 17:32:53 +0000

    update brute force

--- embedx/search.py
+++ embedx/search.py
@@ -23,21 +23,25 @@ class brute_force_search:
             self.text_vectors = np.array(f["embeddings"][:])
 
     def search_image(self, text):
-        """
-        Tìm hình ảnh gần nhất với text_vector bằng cosine similarity
-        Trả về path ảnh phù hợp nhất
-        """
-        # Tính cosine similarity giữa text_vector và tất cả image_vectors
         text_vector = image.embed_Text(text)
         sims = cosine_similarity(text_vector.reshape(1, -1), self.image_vectors)[0]
         best_idx = np.argmax(sims)
         return self.image_paths[best_idx], sims[best_idx]
 
     def search_topK_images(self, text, k=5):
-        """
-        Trả về top-K ảnh có độ tương đồng cosine cao nhất với text_vector
-        """
         text_vector = image.embed_Text(text)
         sims = cosine_similarity(text_vector.reshape(1, -1), self.image_vectors)[0]
         topk_idx = np.argsort(sims)[::-1][:k]
         return [(self.image_paths[i], sims[i]) for i in topk_idx]
+
+    def search_text(self, text):
+        text_vector = text.embed_Text(text)
+        sims = cosine_similarity(text_vector.reshape(1, -1), self.text_vectors)[0]
+        best_idx = np.argmax(sims)
+        return self.text_paths[best_idx], sims[best_idx]
+
+    def search_topK_texts(self, text, k=5):
+        text_vector = text.embed_Text(text)
+        sims = cosine_similarity(text_vector.reshape(1, -1), self.text_vectors)[0]
+        topk_idx = np.argsort(sims)[::-1][:k]
+        return [(self.text_paths[i], sims[i]) for i in topk_idx]
